bc16590521863bf8de854f4fd5e83b087aa34b6a
outp1 <outplayed2511@mail.ru>
2023-02-03 12:55:14 +0300

    bug fixes

--- bot/bot.py
+++ bot/bot.py
@@ -11,13 +11,19 @@ class TelegramChatGPTBotConversation:
     accepted = False
 
     def __init__(
-        self, password: str, bot: Bot, dispatcher: Dispatcher, requester: OpenAIRequest
+        self,
+        password: str,
+        bot: Bot,
+        dispatcher: Dispatcher,
+        requester: OpenAIRequest,
+        admins=[],
     ):
         self.password = password
         self.bot = bot
         self.dispatcher = dispatcher
         self.requester = requester
         self.entering_user_id = None
+        self.admins = [*admins]
 
     async def cmd_start(self, message: types.Message):
         """
@@ -26,16 +32,20 @@ class TelegramChatGPTBotConversation:
         await message.answer(text="Please enter the password")
         self.entering_user_id = message.from_user.id
         self.chat_id = message.chat.id
+        if message.chat.is_forum:
+            self.message_thread_id = message.message_thread_id
 
     async def cmd_stop(self, message: types.Message):
         """
         Conversation's exit point.
         """
-        if message.from_user.id == self.entering_user_id:
+        if message.from_user.id in self.admins:
+            self.accepted = False
             await self.bot.send_message(
-                chat_id=message.chat.id, text="Goodbye! I hope I was helpful."
+                chat_id=message.chat.id,
+                text="Goodbye! I hope I was helpful.",
+                message_thread_id=self.message_thread_id,
             )
-            self.entering_user_id = None
         else:
             # if user is not active, deny access
             await message.answer("You do not have access to stop this conversation.")
@@ -46,9 +56,11 @@ class TelegramChatGPTBotConversation:
         """
         if message.text == self.password:
             self.accepted = True
+            self.admins.append(self.entering_user_id)
             self.entering_user_id = None
             await self.bot.send_message(
                 chat_id=self.chat_id,
+                message_thread_id=self.message_thread_id,
                 text="Password accepted, how can I help you today?",
             )
         else:
@@ -76,14 +88,22 @@ class TelegramChatGPTBotConversation:
             self.cmd_start, commands=["start_conv"], state="*"
         )
         self.dispatcher.register_message_handler(
-            self.cmd_stop, commands=["stop"], state="*"
-        )
-        self.dispatcher.register_message_handler(
             self.handle_password,
             lambda m: m.from_user.id == self.entering_user_id,
         )
         self.dispatcher.register_message_handler(
             self.handle_text,
-            lambda m: m.chat.id == self.chat_id and self.accepted,
+            lambda m: m.chat.id == self.chat_id
+            and (not m.chat.is_forum or m.message_thread_id == self.message_thread_id)
+            and self.accepted
+            and not m.text.startswith("/"),
+            state="*",
+        )
+        self.dispatcher.register_message_handler(
+            self.cmd_stop,
+            lambda m: m.chat.id == self.chat_id
+            and self.accepted
+            and (not m.chat.is_forum or m.message_thread_id == self.message_thread_id),
+            commands=["stop"],
             state="*",
         )
